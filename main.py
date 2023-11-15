cc9779c7c49eac21cdd8710b3b0323b9f1863acd
RwpRwpTwo <rwprwptwo@gmail.com>
2023-11-15 15:54:46 -0500

    Fixed order of main file.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# Function call for main.
-if __name__ == '__main__':
-    main()
-
 # Main function
 def main():
     print("Program executed")
+
+
+# Function call for main.
+if __name__ == '__main__':
+    main()
