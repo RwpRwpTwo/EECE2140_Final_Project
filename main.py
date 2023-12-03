30ad310f2b7b9ce393480249033d17673142b0f4
RwpRwpTwo <rwprwptwo@gmail.com>
2023-12-03 16:29:26 -0500

    Exception catching for main menu.

--- main.py
+++ main.py
@@ -9,22 +9,26 @@ def main():
     fn.read_from_file(my_data, f='data/sample_data_from_lab.csv')
     loop_break = False
     while not loop_break:
-        usr_choice = int(input('What would you like to do?\n\t'
-                               '1. Work with collections\n\t'
-                               '2. Plot & Perform Regressions\n\t'
-                               '3. Import data\n\t'
-                               '4. Exit the program\n'))
-        match usr_choice:
-            case 1:
-                fn.collection_portal(my_data)
-            case 2:
-                fn.plotting_portal(my_data)
-            case 3:
-                fn.import_portal(my_data)
-            case 4:
-                loop_break = True
-            case _:
-                print("That is not a valid input. Try again.")
+        try:
+            usr_choice = int(input('What would you like to do?\n\t'
+                                   '1. Work with collections\n\t'
+                                   '2. Plot & Perform Regressions\n\t'
+                                   '3. Import data\n\t'
+                                   '4. Exit the program\n'))
+            match usr_choice:
+                case 1:
+                    fn.collection_portal(my_data)
+                case 2:
+                    fn.plotting_portal(my_data)
+                case 3:
+                    fn.import_portal(my_data)
+                case 4:
+                    loop_break = True
+                case _:
+                    print("That is not a valid input. Try again.")
+        except Exception as v:
+            print(v)
+            print("Returning to main menu...")
 
 
 # Function call for main.
